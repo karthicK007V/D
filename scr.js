de846e91359bbe8b9bcd198f0b67d065ab204dc2
Karthick V <108870718+karthicK007V@users.noreply.github.com>
2022-07-22 13:08:50 +0530

    Add files via upload

--- scr.js
+++ scr.js
@@ -12,20 +12,28 @@ request.onload=function(){
 
 
       
-//     var res=result.filter((ele)=>ele.region==="Asia").map((ele)=>ele.name);
-//  console.log(res);
-// var  pop=result.filter((ele)=>ele.population<200000).map((ele)=>ele.name);
-// console.log(pop);
-// var name =result.map((ele)=>ele.name);
-// console.log(name);
-// var flag =result.map((ele)=>ele.flag);
-// console.log(flag);
-// var cap =result.map((ele)=>ele.capital);
-// console.log(cap);
-// var population =result.map((ele)=>ele.population).reduce((population,ele)=>population+ele);
-// console.log(population);
-// var cur =result.filter((ele)=>ele.currencies==="USDollars").map((ele)=>ele.name);
-//  console.log(cur);
-//     }
+    var res=result.filter((ele)=>ele.region==="Asia").map((ele)=>ele.name); 
+     console.log(res);
+var  pop=result.filter((ele)=>ele.population<200000).map((ele)=>ele.name);
+console.log(pop);
+var name =result.map((ele)=>ele.name);
+console.log(name);
+var flag =result.map((ele)=>ele.flag);
+console.log(flag);
+var cap =result.map((ele)=>ele.capital);
+console.log(cap);
+var population =result.map((ele)=>ele.population).reduce((population,ele)=>population+ele);
+console.log(population);
+var cur =result.filter((ele)=>
+{
+    for(var i=0;i<ele.currenices.length;i++)
+    {
+        if(ele.currencies[i].code==="USD")
+        {return true}
+    }
+}
+cur=cur.map((ele)=>ele.name);
+ console.log(cur);
+    }
     
     
